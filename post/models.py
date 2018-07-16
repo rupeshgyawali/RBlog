9c6daf0b949730f0a91f6ca1ae928853443a84ea
rupeshgyawali <rupeshgyawali49@gmail.com>
2018-07-16 17:10:20 +0545

    basic detail and create views

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length = 500)
+	content = models.TextField()
+	created = models.DateTimeField(auto_now = False, auto_now_add = True)
+	updated = models.DateTimeField(auto_now = True, auto_now_add = False)
+
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post:details', kwargs = {'pk':self.id})
--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostModelForm
 
 # Create your views here.
+from .models import Post
+
+def index(request):
+	context = {'posts':Post.objects.all() }
+	return render(request, 'post/index.html', context)
+
+def details(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'post/details.html', {'post':post})
+
+def create(request):
+	form = PostModelForm(request.POST or None)
+	if request.method == "POST":
+		newPost = form.save(commit = False)
+		newPost.save()
+		return redirect('post:details', pk = newPost.id)
+
+	return render(request, 'post/create.html', {'form':form})
